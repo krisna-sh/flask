00d1e1366be8d418067d15494ad5db854eb4068f
krisna-sh <krishnakorata04@gmail.com>
2026-02-26 15:48:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template,jsonify
+import mysql.connector
+app = Flask(__name__)
+
+@app.route("/test", methods=["GET"])
+def test():
+    return "<h1> hello </h1>"
+
+@app.route("/test/json", methods=["GET"])
+def testjson():
+    user={"name":"John Doe","age":30,"email":"johndoe@example.com"}
+    return jsonify(user)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/get-data", methods = ["GET"])
+def get_data():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="flask"
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users_auth")
+    data = cursor.fetchall()
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
